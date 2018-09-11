866c89a16acaf75a0a94e6687f4d9055b7e4228e
Nelson de Figueiredo Martins <nelsontecti@gmail.com>
2018-09-11 14:05:32 -0300

    AppCgvf

---
+++ AppCgvf/Models/Pessoas.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppCgvf.Models
+{
+    public class Pessoas
+    {
+        [Key]
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ AppCgvf/Models/PessoasContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace AppCgvf.Models
+{
+    public class PessoasContext : DbContext
+    {
+        public DbSet<Pessoas> Pessoas { get; set; }
+    }
+}
